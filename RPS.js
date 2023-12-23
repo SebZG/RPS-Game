c1f47d5c10e2bf3482549ac8d2230be4ddd8b5d0
SebZG <sebastian.zapata.g@gmail.com>
2023-12-23 20:09:25 +0000

    Refactored to use template literals

--- RPS.js
+++ RPS.js
@@ -1,20 +1,20 @@
 const RPS = () => {
   const choices = ["R", "P", "S"];
   const play = confirm("Do you want to play?");
-  let num = 0;
+  let count = 0;
   let draws = 0;
   let wins = 0;
   let losses = 0;
 
   if (play === true) {
     let rounds = prompt("How many rounds do you want to play?")
-    while (num < rounds) {
-      num++;
-      let rps = prompt("R, P, S ?");
+    while (count < rounds) {
+      count++;
+      let rps = prompt("R, P, S ?").toUpperCase();
       let randomChoice = Math.floor(Math.random() * choices.length);
       let computersChoice = choices[randomChoice];
-      console.log("Your choice was: " + rps);
-      console.log("Computer's choice was: " + computersChoice);
+      console.log(`Your choice was: ${rps}`);
+      console.log(`Computer's choice was: ${computersChoice}`);
       if (rps === computersChoice) {
         draws++;
       } else if ((rps === "R" && computersChoice === "P")
